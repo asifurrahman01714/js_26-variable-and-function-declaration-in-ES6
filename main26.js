bc96534d853e8ab13d68c78f93d381feb7b8ed76
Md.Asif Ur Rahman <45636891+asifurrahman01714@users.noreply.github.com>
2020-09-22 20:05:19 +0600

    Add files via upload

---
+++ main26.js
@@ -0,0 +1,11 @@
+let x =20;
+let y =30;
+let sum = x+y;
+document.write(`the result is ${sum} . the end`);
+
+const add = (x,y) => {
+    let sum =x+y;
+    document.write(`<br> the result is ${sum}`);
+}
+
+add(70,80);
